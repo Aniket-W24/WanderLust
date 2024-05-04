9add5dc4f41c96ade1decb1acc5b2fe01a89a1a7
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-04 20:47:02 +0530

    listing model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js"); //listing schema
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -14,6 +15,25 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
+app.get("/testListing", (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the Beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+  sampleListing
+    .save()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    res.send("testListing Working!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
