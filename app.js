5472ac9b96a4682aaa63c805ff190fe08c83cc35
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-04 22:16:23 +0530

    Added Create Route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,18 +21,40 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 // 😂 Index Route
-app.get("/listings", async (req,res)=> {
-    const allListings = await Listing.find({});       //find all listings
-    res.render("listings/index.ejs", {allListings});
-    // res.send("get listings is workign");
-})
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({}); //find all listings
+  res.render("listings/index.ejs", { allListings });
+  // res.send("get listings is workign");
+});
+
+// 😂 New Route
+app.get("/listings/new", (req, res) => {
+  //this route should be before show route else it will search for new as searching for id in db
+  res.render("listings/new.ejs");
+});
+
+
 
 // 😂 Show Route
-app.get("/listings/:id", async(req,res)=> {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-})
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// 😂 Create Route
+app.post("/listings", (req, res) => {
+    const newListing = new Listing(req.body.listing); //req.body.listing will get all the listing key-value pairs from form where we wrote listing[title],listing[image], etc.
+    newListing
+      .save()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      res.redirect("/listings");
+  });
 
 // 😂 Testing the Database
 app.get("/testListing", (req, res) => {
@@ -51,7 +73,7 @@ app.get("/testListing", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-    res.send("testListing Working!");
+  res.send("testListing Working!");
 });
 
 app.get("/", (req, res) => {
