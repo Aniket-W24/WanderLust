e179e3d0f301210767efebc3a3018a39e54cc367
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-04 21:03:45 +0530

    iamge includes url and filename

--- models/listing.js
+++ models/listing.js
@@ -10,23 +10,28 @@ const listingSchema = new Schema({
     type: String,
   },
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : v,
+    filename: {
+      type: String,
+    },
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v,
+    },
   },
-  price : {
-    type : Number,
-  }, 
-  location : {
-    type : String,
-  }, 
-  country : {
-    type : String,
-  }
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
