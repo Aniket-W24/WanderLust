52697b8d67d4edfb5b2d0bb1669322e9dca84a1b
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-26 23:10:49 +0530

    Marker Popup

--- public/js/map.js
+++ public/js/map.js
@@ -23,10 +23,15 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12',
     // center: [77.2088, 28.6139], // starting position [lng, lat]
-    center: coordinates, // starting position [lng, lat]
+    center: listing.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 const marker = new mapboxgl.Marker({color : "red"})
-    .setLngLat(coordinates)
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset : 25}).setHTML(
+            `<h3>${listing.location}</h3><p>Exact Location will be provided after booking </p>`
+        )
+    )
     .addTo(map);
