0409f6b48d966e9353eeac9ba980650ccb04ce3d
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-26 22:03:12 +0530

    Our First Map

---
+++ public/js/map.js
@@ -0,0 +1,27 @@
+//for google maps
+//   // Step 4: Initialize the map
+//         function initMap() {
+//           // The location of the center of the map
+//           const location = { lat: 28.7 , lng: 77.1 };
+          
+//           // The map, centered at the location
+//           const map = new google.maps.Map(document.getElementById("map"), {
+//             zoom: 8,
+//             center: location,
+//           });
+          
+//           // The marker, positioned at the location
+//           const marker = new google.maps.Marker({
+//             position: location,
+//             map: map,
+//           });
+//         }
+
+//for mapbox
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.2088, 28.6139], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
