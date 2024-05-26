7f6e838ef5d3af5a1ed6182b3814983a2c3e8cfb
Aniket-W24 <aniket.soni.w24@gmail.com>
2024-05-26 22:13:04 +0530

    Geocoding

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing.js");
+//for geocoding
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN
+const geocodingClient = mbxGeocoding({accessToken : mapToken});
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({}); //find all listings
@@ -29,6 +33,13 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient.forwardGeocode({
+    query: req.body.listing.location,
+    limit: 1
+  })
+    .send()
+  console.log(response.body.features[0].geometry.coordinates);
+  res.send("done");
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing); //req.body.listing will get all the listing key-value pairs from form where we wrote listing[title],listing[image], etc.
